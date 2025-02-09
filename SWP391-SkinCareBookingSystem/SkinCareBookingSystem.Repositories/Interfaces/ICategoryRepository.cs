4f4fb1184943c9f6a3ef7e717e5d1ec166f5c3fd
Admin <noik0vshack@gmail.ccom>
2025-02-09 14:27:45 +0700

    Category repo update

---
+++ SWP391-SkinCareBookingSystem/SkinCareBookingSystem.Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using SkinCareBookingSystem.BusinessObject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkinCareBookingSystem.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        public Task<List<Category>> GetCategory();
+        public Task<Category> GetCategoryById(int categoryId);
+        public Task<Category> GetCategoryByName(string categoryName);
+        public void Create(Category category);
+        public void Update(Category category);
+        public Task<bool> Delete(int id);
+        public Task<bool> Savechange();
+        public Task<bool> IsCategoryExist(string categoryName);
+    }
+}
