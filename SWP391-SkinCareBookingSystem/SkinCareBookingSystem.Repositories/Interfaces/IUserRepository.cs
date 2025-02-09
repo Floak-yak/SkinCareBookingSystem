7116b728cf7c78a8908ffeb558e9f39765075fd0
HoangAnh <noik0vshack@gmail.ccom>
2025-02-09 23:59:04 +0700

    User Repository update

---
+++ SWP391-SkinCareBookingSystem/SkinCareBookingSystem.Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using SkinCareBookingSystem.BusinessObject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkinCareBookingSystem.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<List<User>> GetUsers();
+        public Task<User> GetUserById(int userId);
+        public Task<User> GetUserByName(string userName);
+        public void Create(User user);
+        public void Update(User user);
+        public Task<bool> Delete(int userId);
+        public Task<bool> SaveChange();
+        public Task<bool> IsUserExist(string userName);
+    }
+}
