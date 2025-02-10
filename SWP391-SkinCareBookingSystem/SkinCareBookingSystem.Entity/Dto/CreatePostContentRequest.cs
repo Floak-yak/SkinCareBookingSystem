5f1adf26447393071291fbcc68f616fb70c223ff
Khiem <aidolamgidi@gmail.com>
2025-02-10 09:44:51 +0700

    ContentController (Post) Crud

--- SWP391-SkinCareBookingSystem/SkinCareBookingSystem.Entity/Dto/CreatePostContentRequest.cs
+++ SWP391-SkinCareBookingSystem/SkinCareBookingSystem.Entity/Dto/CreatePostContentRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SkinCareBookingSystem.BusinessObject.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SkinCareBookingSystem.BusinessObject.Dto
 {
     public class CreatePostContentRequest
     {
+        public string ContentOfPost { get; set; }
+        public ContentType ContentType { get; set; }
+        public int Position { get; set; }
+        public string imageLink { get; set; }
+        public int PostId { get; set; }
     }
 }
