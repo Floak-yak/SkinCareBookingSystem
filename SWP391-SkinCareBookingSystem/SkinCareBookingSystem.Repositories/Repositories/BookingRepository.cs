9d9aac90bf2c036dd9f9741dff94a62a82080dc3
Khiem <aidolamgidi@gmail.com>
2025-04-08 14:58:43 +0700

    Fix error

--- SWP391-SkinCareBookingSystem/SkinCareBookingSystem.Repositories/Repositories/BookingRepository.cs
+++ SWP391-SkinCareBookingSystem/SkinCareBookingSystem.Repositories/Repositories/BookingRepository.cs
@@ -103,7 +103,7 @@ namespace SkinCareBookingSystem.Repositories.Repositories
         public async Task<List<Booking>> GetCancelBookingByUserId(int userId) =>
             await _context.Bookings
             .Include(b => b.User)
-            .Include(b => b.BookingServiceSchedules)
+            .Include(b => b.BookingServiceSchedules).ThenInclude(bss => bss.Service)
             .OrderByDescending(b => b.CreatedTime)
             .Where(b => b.UserId == userId && b.Status == BookingStatus.Cancel)
             .ToListAsync();
